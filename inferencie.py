f7a2a3de01fe04dc6d8f8535b5b92f72d3db6ede
elton <eltonfernando90@gmail.com>
2024-09-13 15:48:57 -0300

    pre process

--- inferencie.py
+++ inferencie.py
@@ -6,14 +6,14 @@ def preprocess_image(image_path: str, resize: tuple = (64, 64)) -> np.array:
     img = cv2.imread(image_path)
     img = cv2.resize(img, resize)
     img = img.flatten()  # Achatar a imagem em um vetor 1D
-    img = np.array(img, np.float32).reshape(1, -1)  # Converter para float32 e ajustar para forma (1, n_características)
+    img = np.array([img], np.float32)#.reshape(1, -1)  # Converter para float32 e ajustar para forma (1, n_características)
     return img
 
 # Carregar o modelo SVM salvo
 svm = cv2.ml.SVM_load("svm_model.xml")
 
 # Caminho da nova imagem para inferência
-new_image_path = 'yes.jpg'
+new_image_path = 'no.jpg'
 
 # Pré-processar a imagem
 new_image = preprocess_image(new_image_path)
@@ -22,6 +22,7 @@ new_image = preprocess_image(new_image_path)
 result = svm.predict(new_image)[1]
 
 # Mostrar o resultado da inferência
+print("result",result)
 if result[0][0] == 1:
     print("Classe: YES")
 else:
